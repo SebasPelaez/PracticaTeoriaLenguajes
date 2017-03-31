486309241ba1fd3fcbe3139d7a909f6d240cfa37
juanmarin96 <juanes-mejor@hotmail.com>
2017-03-30 21:30:33 -0500

    creación de método que guarda autómata en archivo de texto

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,27 +1,29 @@
 package Main;
 
+import handler.HandlerFile;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.Automata;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        Parent root = FXMLLoader.load(getClass().getResource("Ejemplo.fxml"));
+        /*Parent root = FXMLLoader.load(getClass().getResource("Ejemplo.fxml"));
         primaryStage.setTitle("Peppin World");
         primaryStage.setScene(new Scene(root, 500, 500));
-        primaryStage.show();
+        primaryStage.show();*/
 
 
 
         //ConstruccionAutomata c = new ConstruccionAutomata();
         //c.desplegarVentana();
 
-
+        HandlerFile hf = new HandlerFile(primaryStage);
+        Automata a = hf.crearAutomata();
+        hf.guardarAutomata(a);
+        primaryStage.close();
     }
 
 
